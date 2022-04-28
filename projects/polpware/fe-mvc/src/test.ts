d6983fa804fc4ed799cff8ef5743aed04b63a095
Tom Tang <polpware@gmail.com>
2022-04-28 01:10:35 -0400

    Fix testing

--- projects/polpware/fe-mvc/src/test.ts
+++ projects/polpware/fe-mvc/src/test.ts
@@ -1,22 +1,24 @@
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
 
-import 'core-js/es7/reflect';
 import 'zone.js';
 import 'zone.js/testing';
 import { getTestBed } from '@angular/core/testing';
 import {
-  BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting
+    BrowserDynamicTestingModule,
+    platformBrowserDynamicTesting
 } from '@angular/platform-browser-dynamic/testing';
 
-declare const require: any;
+declare const require: {
+    context(path: string, deep?: boolean, filter?: RegExp): {
+        <T>(id: string): T;
+        keys(): string[];
+    };
+};
 
 // First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(
-  BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting(), {
-    teardown: { destroyAfterEach: false }
-}
+    BrowserDynamicTestingModule,
+    platformBrowserDynamicTesting()
 );
 // Then we find all the tests.
 const context = require.context('./', true, /\.spec\.ts$/);
