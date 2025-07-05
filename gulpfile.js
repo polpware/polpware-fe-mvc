44cc15e2e2171559c7f7b4ee134de82d4541110c
Tomcat <polpware@gmail.com>
2025-07-05 06:51:17 -0400

    Removed doc script

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,4 @@
 const { src, dest, parallel } = require('gulp');
-const typedoc = require("gulp-typedoc");
 const bumpversion = require('gulp-bump');
 
 // `fs` is used instead of require to prevent caching in watch (require caches)
@@ -10,32 +9,6 @@ function getVersion() {
         return data;
     });
 };
-
-function doc() {
-    const newVer = getVersion().trim();
-    
-    return src(["projects/polpware/fe-mvc/src/lib/**/*.ts"])
-        .pipe(typedoc({
-            name: "Polpware typescript mvc " + newVer,            
-            out: "docs/",            
-            
-            module: "commonjs",
-            target: "es5",
-
-            exclude: "projects/polpware/fe-mvc/src/lib/**/*.spec.ts",                        
-
-            experimentalDecorators: true,
-            excludePrivate: true,
-            excludeExternals: true,
-
-            "lib": [
-                "lib.dom.d.ts",
-                "lib.es2015.d.ts",                
-                "lib.es2016.d.ts"
-            ]
-            
-        }));
-}
 
 function bump() {
 
@@ -52,5 +25,4 @@ function bump() {
 }
 
 exports.bump = bump;
-exports.doc = doc;
-exports.default = doc;
+exports.default = bump;
